842924b7d6b0dc2b91f4253e2315536c2f1babb9
Najmiddin Nazirov <nnazirov@transoxiania.com>
2026-08-17 05:34:47 +0500

    expose Tashrif client ID in RootLayout for debugging purposes

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,7 @@ export default function RootLayout({
             <body className="min-h-full flex flex-col">
                 <Tashrif clientId={process.env.NEXT_PUBLIC_TASHRIF_CLIENT_ID!} />
                 <TelegramInit />
+                {process.env.NEXT_PUBLIC_TASHRIF_CLIENT_ID}
                 {children}
             </body>
         </html>
